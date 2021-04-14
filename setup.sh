06df51786e14826f7ce3cc59f19541069361c091
mike.weber <mike.weber@qomplx.com>
2021-04-14 16:33:28 -0600

    v2 of setup.sh

--- setup.sh
+++ setup.sh
@@ -2,20 +2,24 @@
 # 
 # built for Ubuntu 20.04
 #
-sudo apt update all
-sudo apt install openssh-server python2 curl locate git
+sudo apt update
+sudo apt install openssh-server python2 curl locate
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 sudo python2 get-pip.py
 scp qopter@50.19.106.107:Nessus* ./
 scp qopter@50.19.106.107:ptf* ./
+cd ../
 git clone https://github.com/trustedsec/ptf
-cp ptf_custom_list.txt ./ptf/modules/custom_list/list.txt
-sudo ./ptf/ptf <<EOF
+cp qopter_build/ptf_custom_list.txt ./ptf/modules/custom_list/list.txt
+cd ptf
+sudo ./ptf <<EOF
 use modules/custom_list/list/install_update_all
 yes
 EOF
 sudo dpkg -i Nessus*
+cd ../
 git clone https://github.com/trustedsec/tap
-sudo ./tap/setup.py
+cd tap
+sudo ./setup.py
 
 echo "If you didn't get some disastrous error, we should be done. Nessus should be started, but it needs final setup (username and license key).  Tap should have launched... if not, stop the tap service, kill the process manually and restart."
